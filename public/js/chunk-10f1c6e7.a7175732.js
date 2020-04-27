f2d57ded8e42717a89348719b5844588f32318b2
Sergio Escobar <sergioadmira@MacBook-Pro-2.home>
2020-04-27 19:21:13 +0200

    new views 3.0

--- public/js/chunk-10f1c6e7.a7175732.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-10f1c6e7"],{"21bb":function(n,e,t){"use strict";var c=t("d63f"),u=t.n(c);u.a},bb51:function(n,e,t){"use strict";t.r(e);var c=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",{attrs:{id:"space"}})},u=[],a={name:"Home"},s=a,i=(t("21bb"),t("2877")),r=Object(i["a"])(s,c,u,!1,null,null,null);e["default"]=r.exports},d63f:function(n,e,t){}}]);
-//# sourceMappingURL=chunk-10f1c6e7.a7175732.js.map
